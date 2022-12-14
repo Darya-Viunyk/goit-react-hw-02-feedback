800ecb184d3a660d51052e1228c2d129ff0d2a8b
Darya-Viunyk <dar.viunyk@gmail.com>
2022-12-14 15:31:06 +0200

    Added task 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,34 @@
+import { Component } from 'react';
 import Feedback from './Feedback/Feedback';
 
-export const App = () => {
-  return (
-    <div>
-      <Feedback />
-    </div>
-  );
-};
+class App extends Component {
+  state = { good: 0, neutral: 0, bad: 0 };
+
+  onClickButton = ({
+    target: {
+      dataset: { type },
+    },
+  }) => {
+    this.setState({ [type]: this.state[type] + 1 });
+  };
+  render() {
+    const { good, neutral, bad } = this.state;
+    const { onClickButton } = this;
+    return (
+      <div
+        style={{
+          width: 'min-content',
+          margin: '0 auto',
+        }}
+      >
+        <div>
+          <Feedback>
+            options={Object.keys(this.state)}
+            onLeaveFeedback={onClickButton}
+          </Feedback>
+        </div>
+      </div>
+    );
+  }
+}
+export { App };
--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,93 +1,23 @@
-import React from 'react';
+import propTypes from 'prop-types';
+import style from './Feedback.modele.css';
 
-class Feedback extends React.Component {
-  state = {
-    value: 0,
-  };
-  countGoodFeedback = event => {
-    this.setState(goodState => {
-      return {
-        value: goodState.value + 1,
-      };
-    });
-  };
-  countNeutralFeedback = event => {
-    this.setState(neutralState => {
-      return {
-        value: neutralState.value + 1,
-      };
-    });
-  };
-  countBadFeedback = event => {
-    this.setState(badState => {
-      return {
-        value: badState.value + 1,
-      };
-    });
-  };
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    console.log('click');
-  };
-
-  render() {
-    return (
-      <div>
-        <div>
-          <h1>Please leave feedback</h1>
-          <div>
-            <button type="button" onClick={this.countGoodFeedback}>
-              Good
-            </button>
-            <button type="button" onClick={this.countNeutralFeedback}>
-              Neutral
-            </button>
-            <button type="button" onClick={this.countBadFeedback}>
-              Bad
-            </button>
-          </div>
-        </div>
-        <div>
-          <h2>Statistics</h2>
-          <ul>
-            <li>good={this.state.value}</li>
-            <li>neutral={this.state.value}</li>
-            <li>bad={this.state.value}</li>
-            <li>total={this.state.value}</li>
-            <li>positivePercentage={}</li>
-          </ul>
-        </div>
-      </div>
-    );
-  }
+export default function Feedback({ options, onReview }) {
+  return (
+    <div className={style.controls}>
+      {options.map(type => (
+        <button
+          className={style.button}
+          key={type}
+          onClick={onReview}
+          data-type={type}
+        >
+          {type}
+        </button>
+      ))}
+    </div>
+  );
 }
-
-export default Feedback;
-// export const Feedback = () => {
-//   return (
-//     <div>
-//       <div>
-//         <h1>Please leave feedback</h1>
-//         <div>
-//           <button>Good</button>
-//           <button>Neutral</button>
-//           <button>Bad</button>
-//         </div>
-//       </div>
-//       <div>
-//         <h2>Statistics</h2>
-//         <ul>
-//           <li>good={}</li>
-//           <li>neutral={}</li>
-//           <li>bad={}</li>
-//           <li>total={}</li>
-//           <li>positivePercentage={}</li>
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
+Feedback.propTypes = {
+  options: propTypes.arrayOf(propTypes.string).isRequired,
+  onReview: propTypes.func.isRequired,
+};
